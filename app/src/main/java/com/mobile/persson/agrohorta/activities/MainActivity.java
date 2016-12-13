f4cd078b2f8c4f8cca03a96edef73020ddf708eb
Luiz Felipe S. Persson <felipe.persson@gmail.com>
2016-12-13 09:39:32 -0200

    Config project - Added Annotations - Added Firebase

---
+++ app/src/main/java/com/mobile/persson/agrohorta/activities/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mobile.persson.agrohorta.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobile.persson.agrohorta.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/mobile/persson/agrohorta/firebase/FirebaseHelper.java
@@ -0,0 +1,8 @@
+package com.mobile.persson.agrohorta.firebase;
+
+/**
+ * Created by persson on 13/12/16.
+ */
+
+public class FirebaseHelper {
+}
