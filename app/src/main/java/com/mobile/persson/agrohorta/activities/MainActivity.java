6c8f063e2ddbca90a5a17904e53964058b6d7a4f
Luiz Felipe S. Persson <felipe.persson@gmail.com>
2016-12-19 15:39:00 -0200

    Toolbar with profile icon

--- app/src/main/java/com/mobile/persson/agrohorta/activities/MainActivity.java
+++ app/src/main/java/com/mobile/persson/agrohorta/activities/MainActivity.java
@@ -2,14 +2,28 @@ package com.mobile.persson.agrohorta.activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -25,28 +39,109 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
 import com.mobile.persson.agrohorta.R;
 import com.mobile.persson.agrohorta.firebase.FirebaseHelper;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.StringArrayRes;
 
+import java.util.List;
+
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+    //Firebase
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    //Google
+    private GoogleApiClient googleApiClient;
+
     private ProgressDialog progressDialog;
+
+    @ViewById
+    Toolbar toolbar;
+    @ViewById
+    TextView tvToolbarTitle;
+    @ViewById
+    ImageView ivProfile;
 
     @AfterViews
     void initialize() {
+        startDialog();
+        googleAuthConfig();
+        loadToolbar();
 
-        LoginActivity_
-                .intent(this)
-                .flags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                .start();
+        //String name = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
 
+        progressDialog.dismiss();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name;
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            UserInfo user = FirebaseAuth.getInstance().getCurrentUser();
+            loadImageProfile(user.getPhotoUrl());
+            Toast.makeText(this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
+        else
+            ivProfile.setImageResource(R.drawable.ic_account_circle_white_48dp);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        tvToolbarTitle.setText("Catálogo de plantas");
+    }
+
+    private void googleAuthConfig() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    if (user.getDisplayName() != null)
+                        Toast.makeText(getApplicationContext(), user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                } else {
+                    //Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
 
     private void startDialog() {
@@ -54,5 +149,18 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setTitle("buscando data");
         progressDialog.setMessage("aguarde");
         progressDialog.show();
+    }
+
+    private void loadImageProfile(Uri url) {
+        Glide.with(ivProfile.getContext())
+                .load(url)
+                .into(ivProfile);
+    }
+
+    @Click
+    void ivProfile() {
+        LoginActivity_.intent(getApplicationContext())
+                .flags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .start();
     }
 }
