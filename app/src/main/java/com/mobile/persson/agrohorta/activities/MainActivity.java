958d6e715be3497bf6a7e14fce71d63643f539ed
Felipe <luizfelipe@moblee.net>
2017-01-25 21:49:31 -0200

    FIrebase storage download a image

--- app/src/main/java/com/mobile/persson/agrohorta/activities/MainActivity.java
+++ app/src/main/java/com/mobile/persson/agrohorta/activities/MainActivity.java
@@ -2,12 +2,15 @@ package com.mobile.persson.agrohorta.activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,11 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
@@ -40,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     private final static String TAG = "LFSP_DEBUG";
     FirebaseStorage mStorage;
     StorageReference mStorageRef;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private String mDeviceLanguage;
 
@@ -99,8 +108,46 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializeApp() {
         mDeviceLanguage = configApp.getLanguageDevice();
+
+        mAuth = FirebaseAuth.getInstance();
+
         mStorage = FirebaseStorage.getInstance();
         mStorageRef = mStorage.getReferenceFromUrl("gs://agro-horta.appspot.com");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 
     private void googleAuthConfig() {
@@ -152,7 +199,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void teste() {
         StorageReference imagesRef = mStorageRef.child("images");
-        imagesRef.child("onion.png");
+        imagesRef = imagesRef.child("onion.png");
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        imagesRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Data for "images/island.jpg" is returns, use this as needed
+                int i = 0;
+
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                ImageView image = (ImageView) findViewById(R.id.ivIcon);
+
+                image.setImageBitmap(Bitmap.createScaledBitmap(bmp, image.getWidth(),
+                        image.getHeight(), false));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                int i = 0;
+            }
+        });
     }
 
     @Click
@@ -160,5 +228,19 @@ public class MainActivity extends AppCompatActivity {
         LoginActivity_.intent(getApplicationContext())
                 .flags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 .start();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
